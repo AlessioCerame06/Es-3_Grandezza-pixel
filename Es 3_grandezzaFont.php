c4a764e30bd58b4c81d4f1490b9586583bb4516d
AlessioCerame06 <alessiocerame@gmail.com>
2024-09-24 10:21:16 +0200

    Es 3

---
+++ Es 3_grandezzaFont.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>GRANDEZZA FONT</title>
+</head>
+    <body>
+        
+    </body>
+</html>
+
+<?php
+    $nPixel = rand(16, 60);
+    echo "<p style='font-size: $nPixel"."px'>Questo paragrafo è grande $nPixel pixel</p>";
+?>
